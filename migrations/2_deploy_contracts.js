2791b977924f8621d514fd5ae1b8b79907e6a522
KSVSC <samhitaexplorica9@gmail.com>
2019-11-07 19:30:29 +0530

    test cases added

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,5 @@
 var Battleship = artifacts.require("Battleship");
-const SimpleStorage = artifacts.require("SimpleStorage");
-const TutorialToken = artifacts.require("TutorialToken");
-const ComplexStorage = artifacts.require("ComplexStorage");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
-  deployer.deploy(TutorialToken);
-  deployer.deploy(ComplexStorage);
   deployer.deploy(Battleship);
 };
