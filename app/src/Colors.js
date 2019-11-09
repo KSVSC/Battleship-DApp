8c5460e86735f7980d219010ca98c1162af6bdcc
Anurag Jain <ajainuary@gmail.com>
2019-11-09 17:09:33 +0530

    WIP on Placing battleships

--- app/src/Colors.js
+++ app/src/Colors.js
@@ -1,2 +1,3 @@
 export var COLOR_SKY = 'rgb(134, 187, 246)';
 export var COLOR_SEA = 'rgb(35, 116, 171)';
+export var COLOR_BROWN = 'rgb(46, 56, 46)'
--- app/src/Game.js
+++ app/src/Game.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Two from 'two.js';
-import {COLOR_SKY, COLOR_SEA} from './Colors';
+import Button from '@material-ui/core/Button';
+import {COLOR_SKY, COLOR_SEA, COLOR_BROWN} from './Colors';
 class Game extends React.Component {
     constructor(props) {
         super(props);
@@ -10,7 +11,13 @@ class Game extends React.Component {
                 x: 0,
                 y: 0    
             },
-            grid: undefined
+            grid: undefined,
+            stage: 'place',
+            positions: [],
+            ship: {
+                type: 0,
+                orientation: 'h'
+            }
         };
         var params = { width: 410, height: 410 };
         this.two = new Two(params)
@@ -44,28 +51,76 @@ class Game extends React.Component {
     }
 
     componentWillUpdate() {
-        console.log(this.state);
+        console.log(this.state.positions);
         this.two.update();
     }
 
+    placeShip = start => {
+        const type = this.state.ship.type;
+        var positions = this.state.positions;
+        const diff = (this.state.ship.orientation == 'v') ? 1 : 10;
+        for(var i = start; i < start + type*diff && i < 100; i=i+diff) {
+            positions.push({type, i});
+        }
+        return positions;
+    }
+
     handleMouseDown = e => {
-        var x = e.targetTouches ? e.targetTouches[0].clientX : e.clientX;
-        var y = e.targetTouches ? e.targetTouches[0].clientY : e.clientY;
-        var grid = this.state.grid; 
-        grid.forEach(z => {
-            if(z.id == e.target.id) {
-                z.fill = COLOR_SEA;
+        var x = e.clientX;
+        var y = e.clientY;
+        var positions = this.state.positions;
+        if(this.state.stage == 'place') {
+            var start = this.state.grid.findIndex(z => z.id == e.target.id);
+            if(start != -1) {
+                positions = this.placeShip(start);
             }
-        });
+        }
         this.setState({
           cursor: { x, y },
-          grid
+          positions
         });
     }
 
-    
+    handleMouseMove = e => {
+        var x = e.clientX;
+        var y = e.clientY;
+        if(this.state.stage == 'place') {
+            var grid = this.state.grid; 
+            var start = grid.findIndex(z => z.id == e.target.id);
+            if(start != -1) {
+                const type = this.state.ship.type;
+                const diff = (this.state.ship.orientation == 'v') ? 1 : 10;
+                grid.forEach((z, idx) => z.fill = (!this.state.positions.find(a => a.i == idx)) ? COLOR_SKY : COLOR_BROWN);
+                for(var i = start; i < start + type*diff && i < 100; i=i+diff) {
+                    grid[i].fill = COLOR_BROWN;
+                }
+            }
+        }
+        this.setState({cursor: { x, y }});
+    }
+
+    setShip = type => this.setState({
+        ship: {
+            type,
+            orientation: 'h'
+        }
+    });
     render() {
-        return <div ref={this.gameRef} onMouseDown={this.handleMouseDown} onTouchMove={this.handleMouseDown} />;
+        return (<>
+            <Button variant="outlined" onClick={() => this.setShip(4)}>
+                Battleship
+            </Button>
+            <Button variant="outlined" onClick={() => this.setShip(3)}>
+                Destroyer
+            </Button>
+            <Button variant="outlined" onClick={() => this.setShip(2)}>
+                Cruiser
+            </Button>
+            <Button variant="outlined" onClick={() => this.setShip(1)}>
+                Submarine
+            </Button>
+        <div ref={this.gameRef} onMouseDown={this.handleMouseDown} onMouseMove={this.handleMouseMove} />
+        </>);
     }
 }
 
