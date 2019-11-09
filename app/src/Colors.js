00ad4b24662b7a39595ff1e060679808cf001ecf
Anurag Jain <ajainuary@gmail.com>
2019-11-09 14:41:41 +0530

    Game grid working

---
+++ app/src/Colors.js
@@ -0,0 +1,2 @@
+export var COLOR_SKY = 'rgb(134, 187, 246)';
+export var COLOR_SEA = 'rgb(35, 116, 171)';
--- app/src/Game.js
+++ app/src/Game.js
@@ -1,38 +1,71 @@
 import React from 'react';
 import Two from 'two.js';
-
+import {COLOR_SKY, COLOR_SEA} from './Colors';
 class Game extends React.Component {
     constructor(props) {
         super(props);
         this.gameRef = React.createRef();
+        this.state = {
+            cursor: {
+                x: 0,
+                y: 0    
+            },
+            grid: undefined
+        };
+        var params = { width: 410, height: 410 };
+        this.two = new Two(params)
     }
 
     componentDidMount() {
         // Make an instance of two and place it on the page.
         var elem = this.gameRef.current;
-        var params = { width: 285, height: 200 };
-        var two = new Two(params).appendTo(elem);
+        this.two.appendTo(elem);
         // GAME Code goes here !!
-        // two has convenience methods to create shapes.
-        var circle = two.makeCircle(72, 100, 50);
-        var rect = two.makeRectangle(213, 100, 100, 100);
+        var rect = this.two.makeRoundedRectangle(205, 205, 410, 410, 5);
 
-        // The object returned has many stylable properties:
-        circle.fill = '#FF8000';
-        circle.stroke = 'orangered'; // Accepts all valid css color
-        circle.linewidth = 5;
-
-        rect.fill = 'rgb(0, 200, 255)';
+        rect.fill = COLOR_SEA;
         rect.opacity = 0.75;
         rect.noStroke();
+        var grid = new Array(100);
+        for(var i = 0; i < 10; ++i) {
+            for(var j = 0; j < 10; ++j) {
+                var rect = this.two.makeRoundedRectangle(22.75+40.5*i, 22.75 + 40.5*j, 35.5, 35.5, 5);
 
+                rect.fill = COLOR_SKY;
+                rect.opacity = 0.75;
+                rect.noStroke();
+                grid[10*i+j] = rect;
+            }
+        }
         // Don't forget to tell two to render everything
         // to the screen
-        two.update();
+        this.two.update();
+        this.setState({grid});
     }
 
+    componentWillUpdate() {
+        console.log(this.state);
+        this.two.update();
+    }
+
+    handleMouseDown = e => {
+        var x = e.targetTouches ? e.targetTouches[0].clientX : e.clientX;
+        var y = e.targetTouches ? e.targetTouches[0].clientY : e.clientY;
+        var grid = this.state.grid; 
+        grid.forEach(z => {
+            if(z.id == e.target.id) {
+                z.fill = COLOR_SEA;
+            }
+        });
+        this.setState({
+          cursor: { x, y },
+          grid
+        });
+    }
+
+    
     render() {
-        return <div ref={this.gameRef} />;
+        return <div ref={this.gameRef} onMouseDown={this.handleMouseDown} onTouchMove={this.handleMouseDown} />;
     }
 }
 
